95d2ab699470b98048d67c5de3fbc8e3fcd14c4a
George Keith <ggkeithv@gmail.com>
2015-08-31 07:41:44 -0500

    fixed typo in filename

---
+++ complementaryDNA.js
@@ -0,0 +1,5 @@
+//In DNA strings, symbols "A" and "T" are complements of each other, as "C" and "G". You have function with one side of the DNA; you need to get the other complementary side. DNA strand is never empty or there is no DNA at all.
+
+function DNAStrand(dna){
+  //your code here
+}
--- complimentaryDNA.js
+++
@@ -1,5 +0,0 @@
-//In DNA strings, symbols "A" and "T" are complements of each other, as "C" and "G". You have function with one side of the DNA (string, except for Haskell); you need to get the other complementary side. DNA strand is never empty or there is no DNA at all (again, except for Haskell).
-
-function DNAStrand(dna){
-  //your code here
-}
