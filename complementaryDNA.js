59f5d8c13397e3e6d3ddc9509177dd89419bb6f2
George Keith <ggkeithv@gmail.com>
2015-08-31 08:07:32 -0500

    fixed file structure for practice problems

--- complementaryDNA.js
+++
@@ -1,5 +0,0 @@
-//In DNA strings, symbols "A" and "T" are complements of each other, as "C" and "G". You have function with one side of the DNA; you need to get the other complementary side. DNA strand is never empty or there is no DNA at all.
-
-function DNAStrand(dna){
-  //your code here
-}
---
+++ complementaryDNA/complementaryDNA.js
@@ -0,0 +1,22 @@
+//In DNA strings, symbols "A" and "T" are complements of each other, as "C" and "G". You have function with one side of the DNA; you need to get the other complementary side. DNA strand is never empty or there is no DNA at all.
+
+function DNAStrand(dna){
+  //split dna strand into individual chars
+	var splitDNA = dna.split('');
+	//iterate over chars
+	console.log(splitDNA);
+	for (var i = 0; i < splitDNA.length; i++) {
+		//return complementary chars for each provided char
+		if (splitDNA[i] === 'A') {
+			return 'T';
+		} else if (splitDNA[i] === 'C') {
+			return 'G';
+		} else if (splitDNA[i] === 'T') {
+			return 'A';
+		} else if (splitDNA[i] === 'G') {
+			return 'C';
+		}
+	}
+	//join chars back into full complementary dna strand
+  console.log(splitDNA.join(''));
+}
